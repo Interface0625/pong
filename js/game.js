439cf3da2aa5f4681729b623d79e6ff43b37ae32
interface0625 <karolis.lukosius>
2015-08-23 22:45:35 +0200

    implemented POF init3d, warn: Overrides the default game loop,

--- js/game.js
+++ js/game.js
@@ -42,8 +42,49 @@ var game = {
 
         game.context = game.canvas.getContext('2d');  //2d functions enabled
     },
-    init3d: function(){
 
+    init3d: function(){
+        // Setup Canvas:
+        game.canvas = document.querySelector('#myCanvas');
+        game.canvas.width = 1280;
+        game.canvas.height = 720;
+
+        // Setup renderer
+        game.renderer = new THREE.WebGLRenderer({canvas: game.canvas});
+        game.renderer.setSize( game.canvas.width, game.canvas.height );
+        //document.body.appendChild( renderer.domElement );
+
+        // Setup scene:
+        game.scene = new THREE.Scene();
+
+        // Boxes for player, evilAi and ball.
+        game.cube = new THREE.Mesh(
+            new THREE.BoxGeometry( 1, 1, 1 ),
+            new THREE.MeshBasicMaterial( { color: 0x00ff00 } ) );
+        game.scene.add( game.cube );    //  < ---  Pun. Funny... Should have a station where u can play.
+                                        // No worries... Ill probably cross of a box before that
+
+        // Camera.
+        game.camera = new THREE.PerspectiveCamera(
+            75,
+            game.canvas.width / game.canvas.height ,
+            0.1,
+            1000 );
+
+        game.camera.position.z = 5;
+
+        // Lights.
+
+        // Over ride the game loop
+        game.loop = function(timestamp){
+            window.requestAnimationFrame(game.loop);
+            game.update(timestamp);
+
+            game.cube.rotation.x += 0.1;
+            game.cube.rotation.y += 0.1;
+
+            game.renderer.render(game.scene, game.camera);
+        };
     },
 
     loop: function (timestamp) {
